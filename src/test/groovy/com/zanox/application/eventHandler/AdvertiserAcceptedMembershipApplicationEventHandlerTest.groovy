78157f19837251bc10bda2a43da43f52071d6461
Yarek Tyshchenko <yarek.tyshchenko@affiliatewindow.com>
2016-10-17 17:29:03 +0100

    Fix persistence tests

--- src/test/groovy/com/zanox/application/eventHandler/AdvertiserAcceptedMembershipApplicationEventHandlerTest.groovy
+++ src/test/groovy/com/zanox/application/eventHandler/AdvertiserAcceptedMembershipApplicationEventHandlerTest.groovy
@@ -1,24 +1,29 @@
 package com.zanox.application.eventHandler
 
-import com.zanox.application.RedisMapper
 import com.zanox.application.event.AdvertiserAcceptedMembershipApplicationEvent
+import com.zanox.application.model.Membership
+import com.zanox.application.model.MembershipId
+import com.zanox.application.persistence.MembershipRepository
+import com.zanox.application.event.Data
 import spock.lang.Specification
 
 class AdvertiserAcceptedMembershipEventHandlerTest extends Specification {
     def "It persists a message"() {
         setup:
-        def mapper = Mock(RedisMapper)
-        AdvertiserAcceptedMembershipApplicationEventHandler handler = new AdvertiserAcceptedMembershipApplicationEventHandler(mapper)
+        def repository = Mock(MembershipRepository)
+        AdvertiserAcceptedMembershipApplicationEventHandler handler = new AdvertiserAcceptedMembershipApplicationEventHandler(repository)
         def event = Mock(AdvertiserAcceptedMembershipApplicationEvent)
-        event.publisherId = 1337
-        event.advertiserId = 42
+        event.data = new Data()
+        event.data.publisherId = 1337
+        event.data.advertiserId = 42
+        def membership = Mock(Membership)
 
         when:
         handler.handle(event)
 
         then:
-        1 * mapper.persist("42-1337", "accepted")
-        //1 * event.advertiserId
-        //1 * event.publisherId
+        1 * repository.getByKey(_ as MembershipId) >> membership
+        1 * membership.activate() >> membership
+        1 * repository.persist(membership)
     }
 }
