9e8ec45a32a53bd07853a28323c9c52fdb503c43
Yarek Tyshchenko <yarek.tyshchenko@affiliatewindow.com>
2016-10-19 09:04:23 +0100

    Remove more things

--- src/main/java/com/zanox/application/parser/EventParser.java
+++ src/main/java/com/zanox/application/parser/EventParser.java
@@ -11,50 +11,12 @@ import com.zanox.application.eventHandler.AdvertiserAcceptedMembershipApplicatio
 import com.zanox.application.eventHandler.AdvertiserSuspendedMembershipEventHandler;
 import com.zanox.application.eventHandler.PublisherLeftProgrammeEventHandler;
 
-import java.lang.reflect.Type;
-
 public class EventParser{
 
     private Gson gson;
 
     public EventParser() {
         gson = new Gson();
-    }
-
-    public Type getEventClassFromEventName(String eventName) throws DomainEventNotFoundException {
-        switch (eventName) {
-            case "AdvertiserAcceptedMembershipApplicationEvent":
-                return AdvertiserAcceptedMembershipApplicationEvent.class;
-
-        }
-
-        throw new DomainEventNotFoundException(eventName);
-    }
-
-    public DomainEvent getEventFromMessage(byte[] message) throws BadMessageException, UnsupportedEvent {
-        String string = new String(message);
-
-        DomainEvent genericEvent;
-
-        try {
-            genericEvent = gson.fromJson(new String(message), DomainEvent.class);
-        } catch (JsonParseException e) {
-            throw new BadMessageException();
-        }
-
-        Type eventClass;
-        try {
-            eventClass = getEventClassFromEventName(
-                genericEvent.eventName
-            );
-        } catch (DomainEventNotFoundException e) {
-            throw new UnsupportedEvent();
-        }
-
-        return gson.fromJson(
-            string,
-            eventClass
-        );
     }
 
     public void handle(byte[] message) {
