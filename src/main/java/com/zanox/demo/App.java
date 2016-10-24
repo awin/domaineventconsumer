281315a4d68cc34eba6c4a3c5054ab7eef9065be
Yarek Tyshchenko <yarek.tyshchenko@affiliatewindow.com>
2016-10-24 13:33:08 +0100

    Rename zookeeper param

--- src/main/java/com/zanox/demo/App.java
+++ src/main/java/com/zanox/demo/App.java
@@ -6,10 +6,10 @@ public class App {
         Processor processor = new Processor();
         com.zanox.kafka.highlevelconsumer.App app = new com.zanox.kafka.highlevelconsumer.App(processor);
 
-        String zookeeper = args[0];
+        String zkHost = args[0];
         String groupId = args[1];
         String topic = args[2];
         int numTheads = Integer.parseInt(args[3]);
-        app.run(zookeeper, groupId, topic, numTheads);
+        app.run(zkHost, groupId, topic, numTheads);
     }
 }
