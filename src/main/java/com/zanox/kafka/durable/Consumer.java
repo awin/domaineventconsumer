b5bb8bd749d62aae258fd192084d0e13828f9e83
Yarek Tyshchenko <yarek.tyshchenko@awin.com>
2017-10-02 17:06:17 +0100

    Remove deprecated API

--- src/main/java/com/zanox/kafka/durable/Consumer.java
+++ src/main/java/com/zanox/kafka/durable/Consumer.java
@@ -98,42 +98,12 @@ public class Consumer {
     }
 
     /**
-     * Return a List of messages for these partitions and offsets
-     * Offsets are intrinsically attached to their partitions so they always have to be passed together.
-     * Its valid to specify less partitions than are actually available, but other way around throws an exception.
-     * Note: Execution is single-threaded and serial
-     *
-     * @param partitionOffsetMap Partitions and their offsets to consume
-     * @return List of Messages from all selected partitions
-     */
-    @Deprecated
-    public List<Message> getBatchFromPartitionOffset(Map<Integer, Long> partitionOffsetMap) {
-        return getStreamFromPartitionOffset(partitionOffsetMap).collect(Collectors.toList());
-    }
-
-    /**
-     * Get batches of messages as Finite streams
-     * Note: Execution is single-threaded and serial
-     *
-     * @TODO: Implement closing streams
-     * @param partitionOffsetMap Partitions and their offsets
-     * @return Stream of Streams (of messages)
-     */
-    @Deprecated
-    public Stream<Message> getStreamFromPartitionOffset(Map<Integer, Long> partitionOffsetMap) {
-        Stream<Map.Entry<Integer, Long>> stream = partitionOffsetMap.entrySet().stream();
-        return stream.flatMap(entry ->
-            getBatchSupplierForPartitionAndOffset(entry.getKey(), entry.getValue()).get()
-        );
-    }
-
-    /**
      * Get Infinite stream of messages per partition
      * @param partition Partition ID
      * @param offsetLong Offset to start from
      * @return Infinite Stream of messages
      */
-    public Supplier<Stream<Message>> getBatchSupplierForPartitionAndOffset(int partition, Long offsetLong) {
+    private Supplier<Stream<Message>> getBatchSupplierForPartitionAndOffset(int partition, Long offsetLong) {
         Broker leader = getLeaderForPartition(partition);
         MessageConsumer messageConsumer = this.kafkaConsumerFactory.messageConsumer(leader);
 
