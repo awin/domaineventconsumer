c03c52f6412ae3efd4145a2781b1896650561d46
Yarek Tyshchenko <yarek.tyshchenko@awin.com>
2018-02-22 12:38:22 +0000

    Implement toString and other bits on Offset object

--- src/main/java/com/zanox/kafka/durable/Offset.java
+++ src/main/java/com/zanox/kafka/durable/Offset.java
@@ -14,4 +14,24 @@ public class Offset {
     public Offset(long time) {
         value = time;
     }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Offset offset = (Offset) o;
+
+        return value == offset.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (value ^ (value >>> 32));
+    }
 }
